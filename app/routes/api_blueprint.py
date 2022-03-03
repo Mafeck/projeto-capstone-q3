ac1672d70d3085bbc5e06af3f7a0437b1e3b1896
FelipeMatheus1999 <felipe_cavalcante7939@hotmail.com>
2022-03-03 18:16:29 -0300

    Added changes

--- app/routes/api_blueprint.py
+++ app/routes/api_blueprint.py
@@ -4,4 +4,4 @@ from app.routes.route_clients_blueprint import bp_clients
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
 
-bp_api.register(bp_clients)
+bp_api.register_blueprint(bp_clients)
