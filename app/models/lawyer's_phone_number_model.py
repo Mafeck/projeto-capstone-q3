08faa96d28cbbca3507f3751011bc931e1d12300
Felipe Matheus Rabelo Cavalcante <85685562+FelipeMatheus1999@users.noreply.github.com>
2022-03-07 09:23:27 -0300

    Update lawyer's_phone_number_model.py

--- app/models/lawyer's_phone_number_model.py
+++ app/models/lawyer's_phone_number_model.py
@@ -12,12 +12,12 @@ class LawyersPhoneNumber(db.Model):
     phone: str
     lawyer_id: int
 
-    __tablename__ = "Lawyer's_phone_number"
+    __tablename__ = "lawyer's_phone_number"
 
     id = Column(Integer, primary_key=True)
     phone = Column(String, nullable=False, unique=True)
-    lawyer_id = Column(Integer, ForeignKey("Lawyers.id"))
+    lawyer_id = Column(Integer, ForeignKey("lawyers.id"))
 
     lawyers = relationship(
-        "LawyerModel", backref=backref("Lawyer's_phone_number", uselist=True), uselist=False
+        "LawyerModel", backref=backref("lawyer's_phone_number", uselist=True), uselist=False
     )
