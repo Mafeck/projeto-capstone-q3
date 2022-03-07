b631eb3ec9914a178e60d0dca77091ac5907d66c
Felipe Matheus Rabelo Cavalcante <85685562+FelipeMatheus1999@users.noreply.github.com>
2022-03-07 09:21:35 -0300

    Update lawyers_clients_table.py

--- app/models/lawyers_clients_table.py
+++ app/models/lawyers_clients_table.py
@@ -3,6 +3,6 @@ from app.configs.database import db
 
 lawyers_clients_table = db.Table('lawyers_clients_table',
                                  db.Column('id', db.Integer, primary_key=True),
-                                 db.Column('lawyer_oab', db.Integer, nullable=False),
-                                 db.Column('client_cpf', db.Integer, nullable=False)
+                                 db.Column('lawyer_oab', db.Integer, db.ForeignKey("lawyers.oab")),
+                                 db.Column('client_cpf', db.Integer, db.ForeignKey("clients.cpf"))
                                  )
